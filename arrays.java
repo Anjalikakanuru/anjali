0f2c0ecbe8634100b546f582cda2c65699b29331
Anjalikakanuru <165399616+Anjalikakanuru@users.noreply.github.com>
2024-07-29 07:56:34 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,12 @@
+class arrays
+{
+public static void main(String args[])
+{
+int arr[]=new int[]{2,3,4,5,6};
+for(int i=1;i<arr.length;i++)
+{
+System.out.println("the value of array"+arr[i]);
+}
+}
+}
+
